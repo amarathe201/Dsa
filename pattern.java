a56fb2a3950114d328d133b5ec4775e32c930302
AMOL  MARATHE <138742505+amarathe201@users.noreply.github.com>
2024-07-21 11:25:14 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,87 +1,93 @@
 public class pattern {
-
-    // public static void halfinpt(int n){
-    //     for (int i=1; i<=n ; i++){
+    
+// Code for Half Inverted Pattern
+    public static void half_inverted_pt(int n){
+        for (int i=1; i<=n ; i++){
             
-    //         // Spaces
-    //         for (int j=1; j<=n-i; j++){
-    //             System.out.println(" ");
-    //         }
+            // Spaces
+            for (int j=1; j<=n-i; j++){
+                System.out.println(" ");
+            }
 
-    //         //Stars
-    //         for (int j=1; j<=i ; j++){
-    //             System.out.println("*");
-    //         }
-    //         System.out.println();
-    //     }
+            //Stars
+            for (int j=1; j<=i ; j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
         
-    // }
+    }
 
-    // public static void numpt(int n ){
-    //     for (int i=1; i<=n ; i++){
-    //         for (int j=1; j<=n-i+1; j++){
-    //             System.out.print(j);
-    //         }
-    //         System.out.println();
-    //     }
-    // }
+// Code for Number Pattern
+    public static void number_pt(int n ){
+        for (int i=1; i<=n ; i++){
+            for (int j=1; j<=n-i+1; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
 
-    // public static void floytr(int n){
-    //     int counter = 1;
+// Code for Floyd's trinagle 
+    public static void floyd_tri(int n){
+        int counter = 1;
 
-    //     for (int i=1; i<=n; i++){
-    //         for (int j=1; j<=i; j++){
-    //             System.out.print(counter);
-    //             counter++;
-    //         }
-    //         System.out.println();
-    //     }
-    // }
-
-
-    // public static void rhombus(int n){
-
-    //     for (int i=1; i<=n; i++){
-
-    //         //spaces
-    //         for (int j=1; j<=n-i; j++){
-    //             System.out.print(" ");
-    //         }
-
-    //         //stars
-    //         for(int j=1; j<=n; j++){
-    //             System.out.print("*");
-    //         }
-
-    //         System.out.println();
-    //     }
-
-    // }
-
-    // public static void holl_rhombus(int n){
-    //     for(int i=1; i<=n; i++){
-
-    //         //space-outer
-    //         for(int j=1; j<=n-i; j++){
-    //             System.out.print(" ");
-    //         }  
-
-    //         //hollow-rectangle -- stars
-    //         for (int j=1; j<=n; j++){
-    //             if (i==1 || i==n || j==1 || j==n){
-    //                 System.out.print("*");
-    //             }
-    //             else {
-    //                 System.out.print(" ");
-    //             }
-    //         }
-    //         System.out.println();
-    //     }
-
-    // }
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print(counter);
+                counter++;
+            }
+            System.out.println();
+        }
+    }
 
 
-    public static void diamond(int n){
+// Code for the Rhombus Pattern
+    public static void rhombus_pt(int n){
+
+        for (int i=1; i<=n; i++){
+
+            //spaces
+            for (int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            //stars
+            for(int j=1; j<=n; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+//Code for Hollow Rhombus Pattern
+    public static void holl_rhombus_pt(int n){
+        for(int i=1; i<=n; i++){
+
+            //space-outer
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }  
+
+            //hollow-rectangle -- stars
+            for (int j=1; j<=n; j++){
+                if (i==1 || i==n || j==1 || j==n){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+
+//Code for Diamond Pattern
+    public static void diamond_pt(int n){
 
         // 1st half ( or upper part)
         for (int i=1; i<=n; i++){
@@ -121,12 +127,12 @@ public class pattern {
 
 
     public static void main(String[] args) {
-        // halfinpt(5);
-        // numpt(5);
-        // floytr(6);
-        // rhombus(5);
-        // holl_rhombus(5);
-        diamond(4);
+        half_inverted_pt(5);
+        number_pt(5);
+        floyd_tri(6);
+        rhombus_pt(5);
+        holl_rhombus_pt(5);
+        diamond_pt(4);
     }
     
 }
